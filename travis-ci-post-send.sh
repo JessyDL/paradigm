d3d3c64b7644193cee35a4b677d869ffa3a2e474
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2018-11-20 11:10:28 +0200

    another attemp at fixing the expansion

--- travis-ci-post-send.sh
+++ travis-ci-post-send.sh
@@ -51,7 +51,7 @@ case $1 in
       if [[ ${#UTESTS_TXT} > 1024 ]]; then
         UTESTS_TXT=${UTESTS_TXT:0:1024}
       fi
-	   UTESTS_TXT=${UTESTS_TXT//\"/\'}
+	   UTESTS_TXT=${UTESTS_TXT//\'/\"}
 	   UTESTS_TXT=${UTESTS_TXT//'\n'/\\n}
       UNIT_TEST_RESULTS=', "fields": [{"name": "Unit Tests - Passed", "value": "'"$UTESTS_TXT"'" } ]'
     else 
@@ -64,7 +64,7 @@ case $1 in
       if [[ ${#UTESTS_TXT} > 1024 ]]; then
         UTESTS_TXT=${UTESTS_TXT:0:1024}
       fi
-	   UTESTS_TXT=${UTESTS_TXT//\"/\'}
+	   UTESTS_TXT=${UTESTS_TXT//\'/\"}
 	   UTESTS_TXT=${UTESTS_TXT//'\n'/\\n}
       UNIT_TEST_RESULTS=', "fields": [{"name": "Unit Tests - Failed", "value": "'"$UTESTS_TXT"'" } ]'
     else 
@@ -73,6 +73,8 @@ case $1 in
     ;;
 esac
 	
+echo -e "$UNIT_TEST_RESULTS"
+
 WEBHOOK_DATA='{
   "avatar_url": "https://travis-ci.org/images/logos/TravisCI-Mascot-1.png",
   "embeds": 
@@ -84,7 +86,7 @@ WEBHOOK_DATA='{
     "timestamp": "'"$TIMESTAMP"'",
     "url": "'"$COMMIT_URL"'",
     "footer": { "text": "'"$CREDITS"'" }
-    '$UNIT_TEST_RESULTS'
+    '"$UNIT_TEST_RESULTS"'
   }]}'
 	
 echo -e "$WEBHOOK_DATA"
