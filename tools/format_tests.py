0e69e59a7849862aa666070ace94368b0ee8a6a6
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2018-11-21 17:57:30 +0200

    fixed doxygen file unit test formatter tweaked to remove all quotes

--- tools/format_tests.py
+++ tools/format_tests.py
@@ -1,6 +1,6 @@
 import sys
 with open(sys.argv[1]) as f:
-  alist = [line.rstrip().replace("'", '`') for line in f]
+  alist = [line.rstrip().replace("'", '`').replace('"', '`') for line in f]
 	
 with open(sys.argv[2], 'w') as f:
   for item in alist:
