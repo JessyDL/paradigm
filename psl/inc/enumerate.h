0e946230ea4a20545b1bd953c121790a8c13b0f2
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2018-11-26 18:56:34 +0200

    added array_view type

--- psl/inc/enumerate.h
+++ psl/inc/enumerate.h
@@ -75,6 +75,82 @@ namespace psl
 		  index_type count;
 	};
 
+	template<typename T>
+	class array_view
+	{
+	public:
+		class iterator
+		{
+		public:
+			iterator(T* iterator) noexcept : it(iterator) {}
+			~iterator() = default;
+			iterator(const iterator& other) noexcept = default;
+			iterator(iterator&& other) noexcept = default;
+			iterator& operator=(const iterator& other) noexcept = default;
+			iterator& operator=(iterator&& other) noexcept = default;
+
+			iterator& operator++()
+			{
+				++it;
+				return *this;
+			}
+
+			iterator& operator--()
+			{
+				--it;
+				return *this;
+			}
+
+			iterator operator++() const
+			{
+				auto copy{*this};
+				++copy;
+				return copy;
+			}
+			iterator operator--() const
+			{
+				auto copy{*this};
+				--copy;
+				return copy;
+			}
+
+			bool operator!=(const iterator &other) const noexcept { return it != other.it; }
+
+			bool operator==(const iterator &other) const noexcept { return it == other.it; }
+
+			T& operator*()
+			{
+				return *it;
+			}
+
+
+			const T& operator*() const
+			{
+				return *it;
+			}
+		private:
+			T* it;
+		};
+		template<typename container_t>
+		array_view(container_t& container) : first(std::begin(container)), last(std::end(container)) {};
+		array_view(T* first, T* last) : first(first), last(last) {};
+
+		~array_view() = default;
+		array_view(const iterator& other) noexcept = default;
+		array_view(iterator&& other) noexcept = default;
+		array_view& operator=(const array_view& other) noexcept = default;
+		array_view& operator=(array_view&& other) noexcept = default;
+
+		iterator begin() const { return iterator(first); }
+
+		iterator end() const { return iterator(last); }
+
+		size_t size() const { return last - first;}
+	private:
+		T* first;
+		T* last;
+	};
+
 	template <class T>
 	auto enumerate(T first, T last, typename enumerator<T>::index_t index = 0) -> enumerator<T>
 	{
