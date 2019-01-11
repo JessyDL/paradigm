c8369cfa05eb83a1cbde4e8db9075eec4cef8c2c
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2019-01-11 22:09:45 +0200

    moved SSE instructions for matrices into the default setup

---
+++ psl/inc/math/AVX2/matrix.h
@@ -0,0 +1,8 @@
+#pragma once
+#if INSTRUCTION_SET == 3
+#undef INSTRUCTION_SET
+#define INSTRUCTION_SET 2
+#include "math/AVX/matrix.h"
+#undef INSTRUCTION_SET
+#define INSTRUCTION_SET 3
+#endif
--- psl/inc/math/matrix.h
+++ psl/inc/math/matrix.h
@@ -216,50 +216,11 @@ namespace psl
 	using dmat1x1   = tmat<double, 1, 1>;
 	using imat1x1   = tmat<int, 1, 1>;
 	using mat1x1_sz = tmat<size_t, 1, 1>;
-
-	template <typename precision_t, size_t d1, size_t d2, size_t d3>
-	constexpr tmat<precision_t, d1, d3> operator*(const tmat<precision_t, d1, d2>& left,
-												  const tmat<precision_t, d2, d3>& right) noexcept
-	{
-		tmat<precision_t, d1, d3> res{1};
-		if constexpr(d1 * d3 == 16)
-		{
-			__m128 a_line, b_line, r_line;
-			for(int i = 0; i < 16; i += 4)
-			{
-				// unroll the first step of the loop to avoid having to initialize r_line to zero
-				a_line = _mm_load_ps(left.value.data());         // a_line = vec4(column(a, 0))
-				b_line = _mm_set1_ps(right[i]);      // b_line = vec4(b[i][0])
-				r_line = _mm_mul_ps(a_line, b_line); // r_line = a_line * b_line
-				for(int j = 1; j < 4; j++)
-				{
-					a_line = _mm_load_ps(&left[j * 4]); // a_line = vec4(column(a, j))
-					b_line = _mm_set1_ps(right[i + j]);  // b_line = vec4(b[i][j])
-												   // r_line += a_line * b_line
-					r_line = _mm_add_ps(_mm_mul_ps(a_line, b_line), r_line);
-				}
-				_mm_store_ps(&res[i], r_line);     // r[i] = r_line
-			}
-		}
-		else
-		{
-			for(size_t column = 0; column < d1; column++)
-			{
-				for(size_t row = 0; row < d3; row++)
-				{
-					res[{row, column}] = precision_t{0};
-					for(size_t p = 0; p < d2; p++)
-					{
-						res[{row, column}] += left[{p, column}] * right[{row, p}];
-					}
-				}
-			}
-		}
-		return res;
-	}
-
 } // namespace psl
 
 
-namespace psl::math
-{}
+
+#include "math/AVX2/matrix.h"
+#include "math/AVX/matrix.h"
+#include "math/SSE/matrix.h"
+#include "math/fallback/matrix.h"
