21e7165d8ac018efefceff03ff3bd1040614c34d
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2019-07-04 23:11:20 +0300

    Added new files to the inc/src for cmake

--- psl/inc/view_ptr.h
+++ psl/inc/view_ptr.h
@@ -14,9 +14,9 @@ namespace psl
 	class view_ptr
 	{
 	  public:
-		using element_type	= T;
-		using pointer		  = T*;
-		using reference		  = T&;
+		using element_type = T;
+		using pointer	  = T*;
+		using reference	= T&;
 
 		constexpr view_ptr() noexcept = default;
 		constexpr explicit view_ptr(pointer value) noexcept : m_Value(value) {}
@@ -25,15 +25,34 @@ namespace psl
 		constexpr view_ptr(const psl::unique_ptr<T2>& value) noexcept : m_Value(&value.get())
 		{}
 
-		~view_ptr()						= default;
-		view_ptr(const view_ptr& other) = default;
-		view_ptr(view_ptr&& other)		= default;
-		view_ptr& operator=(const view_ptr& other) = default;
-		view_ptr& operator=(view_ptr&& other) = default;
+		~view_ptr() = default;
+		view_ptr(const view_ptr& other) : m_Value(other.m_Value){};
+		view_ptr(view_ptr&& other) noexcept : m_Value(other.m_Value) { other.m_Value = nullptr; };
+		view_ptr& operator=(const view_ptr& other)
+		{
+			if(this != &other)
+			{
+				m_Value = other.m_Value;
+			}
+			return *this;
+		};
+		view_ptr& operator=(view_ptr&& other) noexcept
+		{
+			if(this != &other)
+			{
+				m_Value		  = other.m_Value;
+				other.m_Value = nullptr;
+			}
+			return *this;
+		};
 
 		constexpr pointer get() const noexcept { return m_Value; }
 
-		constexpr reference operator*() const { return assert(m_Value != nullptr), *m_Value; }
+		constexpr reference operator*() const noexcept
+		{
+			assert_debug_break(m_Value != nullptr);
+			return *m_Value;
+		}
 
 		constexpr pointer operator->() const noexcept { return m_Value; }
 
@@ -96,8 +115,20 @@ namespace psl
 	}
 } // namespace psl
 
+template<typename T>
+constexpr static void swap(psl::view_ptr<T>& lhs, psl::view_ptr<T>& rhs) noexcept
+{
+	lhs.swap(rhs);
+}
+
 namespace std
 {
+	template <typename T>
+	constexpr static void swap(psl::view_ptr<T>& lhs, psl::view_ptr<T>& rhs) noexcept
+	{
+		lhs.swap(rhs);
+	}
+
 	template <class T>
 	struct hash<::psl::view_ptr<T>>
 	{
