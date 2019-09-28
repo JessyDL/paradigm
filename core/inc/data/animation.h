e764a37c14e86a32e727eec46fb6c89c320be187
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2019-09-28 21:12:28 +0300

    [data] draft of animation file

--- core/inc/data/animation.h
+++ core/inc/data/animation.h
@@ -11,11 +11,40 @@ namespace core::data
 {
 	class animation
 	{
-		friend class psl::serialization::accessor;
 
 		template <typename T, char... Char>
 		using prop = psl::serialization::property<T, Char...>;
+
 	  public:
+		friend class psl::serialization::accessor;
+		class bone
+		{
+		  public:
+			friend class psl::serialization::accessor;
+			bone()  = default;
+			~bone() = default;
+
+			bone(const bone& other)		= default;
+			bone(bone&& other) noexcept = default;
+			bone& operator=(const bone& other) = default;
+			bone& operator=(bone&& other) noexcept = default;
+
+			psl::string8::view name() const noexcept;
+			void name(psl::string8_t value) noexcept;
+			
+		  private:
+			template <typename S>
+			void serialize(S& serializer)
+			{
+				serializer << m_Name;
+			};
+
+			static constexpr const char serialization_name[5]{"BONE"};
+
+			prop<psl::string8_t, const_str("NAME", 4)> m_Name;
+		};
+		animation(core::resource::cache& cache, const core::resource::metadata& metaData,
+				  psl::meta::file* metaFile) noexcept;
 		animation()  = default;
 		~animation() = default;
 
@@ -24,11 +53,29 @@ namespace core::data
 		animation& operator=(const animation& other) = default;
 		animation& operator=(animation&& other) noexcept = default;
 
+		/// \brief returns the expected time in seconds for this animation
+		/// \details using the current set duration, and current set fps
+		/// this method will return the expected time in seconds to cycle the animation
+		/// \returns time in seconds
+		float seconds() const noexcept;
+
+		psl::string8::view name() const noexcept;
+		void name(psl::string8_t value) noexcept;
+
+		uint32_t duration() const noexcept;
+		void duration(uint32_t value) noexcept;
+
+		uint32_t fps() const noexcept;
+		void fps(uint32_t value) noexcept;
+
+		psl::array_view<bone> bones() const noexcept;
+		void bones(psl::array<bone> value) noexcept;
+
 	  private:
 		template <typename S>
 		void serialize(S& serializer)
 		{
-			serializer << m_Name << m_Duration << m_Fps;
+			serializer << m_Name << m_Duration << m_Fps << m_Bones;
 		};
 
 
@@ -37,5 +84,6 @@ namespace core::data
 		prop<psl::string8_t, const_str("NAME", 4)> m_Name;
 		prop<uint32_t, const_str("DURATION", 8)> m_Duration;
 		prop<uint32_t, const_str("FPS", 3)> m_Fps;
+		prop<psl::array<bone>, const_str("BONE", 4)> m_Bones;
 	};
-}
+} // namespace core::data
--- core/src/data/animation.cpp
+++ core/src/data/animation.cpp
@@ -1,1 +1,31 @@
 #include "data/animation.h"
+#include "resource/resource.hpp"
+
+using namespace core::data;
+using namespace core::resource;
+
+
+animation::animation(core::resource::cache& cache, const core::resource::metadata& metaData,
+					 psl::meta::file* metaFile) noexcept
+{}
+
+float animation::seconds() const noexcept
+{
+	return static_cast<float>(m_Duration.value) / static_cast<float>(m_Fps.value);
+}
+
+psl::string8::view animation::name() const noexcept { return m_Name.value; }
+void animation::name(psl::string8_t value) noexcept { m_Name.value = std::move(value); }
+
+uint32_t animation::duration() const noexcept { return m_Duration.value; }
+void animation::duration(uint32_t value) noexcept { m_Duration.value = value; }
+
+uint32_t animation::fps() const noexcept { return m_Fps.value; }
+void animation::fps(uint32_t value) noexcept { m_Fps.value = value; }
+
+
+psl::array_view<animation::bone> animation::bones() const noexcept { return m_Bones.value; }
+void animation::bones(psl::array<animation::bone> value) noexcept { m_Bones = std::move(value); }
+
+psl::string8::view animation::bone::name() const noexcept { return m_Name.value; }
+void animation::bone::name(psl::string8_t value) noexcept { m_Name.value = std::move(value); }
