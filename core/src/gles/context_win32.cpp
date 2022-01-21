808379e7f7c2b2e35a55ea55f75460e1028c110b
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2022-01-21 16:03:29 +0200

    [core] split gles/context into surface specific files

--- core/src/gles/context_win32.cpp
+++ core/src/gles/context_win32.cpp
@@ -1,18 +1,20 @@
+
+#ifdef SURFACE_WIN32
+
 #include "gles/context.hpp"
 #include "gfx/limits.hpp"
-#ifdef PLATFORM_WINDOWS
 #include "glad/glad_wgl.h"
 #include <Windows.h>
-#endif
 #include "logging.hpp"
 #include "os/surface.hpp"
+
+using namespace core;
+using namespace core::igles;
+
 
 HDC target;
 HWND hwnd;
 HGLRC rc;
-
-using namespace core;
-using namespace core::igles;
 
 void* glGetProcAddress(const char* name)
 {
@@ -289,5 +291,5 @@ void context::enable(const core::os::surface& surface)
 	}
 }
 
-
 bool context::swapbuffers(core::resource::handle<core::os::surface> surface) { return SwapBuffers(target); }
+#endif
---
+++ core/src/gles/context_xcb.cpp
@@ -0,0 +1,32 @@
+
+#if defined(SURFACE_XCB)
+
+#include "gles/context.hpp"
+#include "gfx/limits.hpp"
+#include "logging.hpp"
+#include "os/surface.hpp"
+
+using namespace core;
+using namespace core::igles;
+
+
+context::context(core::resource::cache_t& cache,
+				 const core::resource::metadata& metaData,
+				 psl::meta::file* metaFile,
+				 psl::string8::view name)
+{
+
+
+}
+
+context::~context()
+{
+
+}
+void context::enable(const core::os::surface& surface)
+{
+
+
+}
+bool context::swapbuffers(core::resource::handle<core::os::surface> surface) { return false; }
+#endif
