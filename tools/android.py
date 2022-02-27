8e27267d967586ebfdbe2a00f5bc3f7d6238658a
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2022-02-27 16:09:53 +0200

    Cleaned up android project generation

--- tools/android.py
+++ tools/android.py
@@ -1,139 +1,181 @@
 from argparse import ArgumentParser
+from email.mime import application
 import io
 import os
 import shutil
 import subprocess
+from distutils.dir_util import copy_tree
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 ROOT_DIR = os.path.join(CURRENT_DIR, '..')
 
 def run_command(command=[], directory=None, print_stdout=False, catch_stdout=False):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=directory)
+    process = subprocess.Popen(command, stdout=os.sys.stdout if print_stdout and not catch_stdout else subprocess.PIPE, stderr=os.sys.stderr, cwd=directory)
     output = []
-    if print_stdout:
-        for line in io.TextIOWrapper(process.stdout, newline=''):
-            if not line.endswith('\r') and catch_stdout:
-                output.append(line[:len(line)-1] if line[-1] == os.linesep else line)
-    elif catch_stdout:
-        for line in io.TextIOWrapper(process.stdout, newline=os.linesep):
-            line = line.rsplit('\r', maxsplit=1)[-1]
-            output.append(line[:len(line)-1])
-    process.stdout.close()
+    if catch_stdout:
+        if print_stdout:
+            for line in io.TextIOWrapper(process.stdout, newline=''):
+                if not line.endswith('\r'):
+                    output.append(line[:len(line)-1] if line[-1] == os.linesep else line)
+        else:
+            for line in io.TextIOWrapper(process.stdout, newline=os.linesep):
+                line = line.rsplit('\r', maxsplit=1)[-1]
+                output.append(line[:len(line)-1])
+        process.stdout.close()
     process.wait()
     if process.returncode != 0:
         raise Exception(f"Raised exitcode '{process.returncode}' while trying to run the command '{' '.join(comm for comm in command)}'")
     return output
 
-def generate(directory, project="main"):
-    os.makedirs(directory, exist_ok=True)
-    if not os.path.exists(os.path.join(directory, 'settings.gradle')):
-        run_command(command=[
-                'gradle', 'init',
-                '--type', 'basic',
-                '--dsl', 'groovy',
-                '--project-name', project,
-                '--incubating'
-            ], directory=directory, print_stdout=True)
+class Android:
+    def __init__(self, directory, sdk=None, gradle=None, bundletool=None) -> None:
+        self._sdk = sdk
+        if self._sdk != None and self._sdk != "":
+            os.environ["ANDROID_SDK_ROOT"] = self._sdk
+        self._gradle = gradle or ""
+        self._bundletool = bundletool or ""
+        self._directory = directory
+        self._generated = os.path.exists(directory) and os.path.exists(os.path.join(directory, 'build.gradle'))
+        self._regenerate_apks = set()
+        packages = Android._parse_installed_packages(channel=3, sdk=self._sdk)
+        for dependency, channel in Android.dependencies():
+            Android._install_required(packages, dependency, channel, sdk=self._sdk)
 
-    android_templates = os.path.join(CURRENT_DIR, 'templates', 'android')
-
-    templates = [os.path.join(dp, f) for dp, dn, filenames in os.walk(android_templates) for f in filenames if os.path.splitext(f)[1] == '.template']
-
-    for template in templates:
-        os.makedirs(os.path.dirname(os.path.join(directory, os.path.relpath(template, android_templates))), exist_ok=True)
-        with open(template, 'r') as template_file, open(os.path.join(directory, os.path.relpath(template, android_templates).rsplit('.', maxsplit=1)[0]), 'w') as file:
-            template = template_file.read()
-            template = template.replace('{{PROJECT}}', project)
-            template = template.replace('{{COMPANY}}', "paradigmengine")
-            file.write(template)
-
-    resources = [os.path.join(dp, f) for dp, dn, filenames in os.walk(android_templates) for f in filenames if os.path.splitext(f)[1] != '.template']
-    for resource in resources:
-        os.makedirs(os.path.dirname(os.path.join(directory, os.path.relpath(resource, android_templates))), exist_ok=True)
-        with open(resource, 'rb') as resource_file, open(os.path.join(directory, os.path.relpath(resource, android_templates)), 'wb') as file:
-            file.write(resource_file.read())
-
-    # create the symlinks to the source code
-    cpp_dir = os.path.join(directory, 'app', 'src', 'main')
-    [ os.symlink(os.path.join(ROOT_DIR, dir), os.path.join(cpp_dir, dir)) for dir in ['psl', 'core', 'extern', 'cmake', 'CMakeLists.txt', 'tools'] if not os.path.exists(os.path.join(cpp_dir, dir))]
-
-def parse_installed_packages(channel=0):
-    sdkman_output = run_command(["sdkmanager", "--list", f"--channel={str(channel)}"], catch_stdout=True)
-    
-    packages = {'installed': {}, 'available': {}}
-    for line in [package for package in sdkman_output if package.count('|') == 3]:
-        name, version, _, location = (l.strip() for l in line.split('|'))
-        if name == "Path" or name.startswith('-'):
-            continue
-        packages['installed'][name] = {'version': version, 'location': location}
-
-    for line in [package for package in sdkman_output if package.count('|') == 2]:
-        name, version, _ = (l.strip() for l in line.split('|'))
-        if name == "Path" or name.startswith('-'):
-            continue
-        packages['available'][name] = {'version': version }
-
-    return packages
-
-def install_required_packages():
-    packages = parse_installed_packages(channel=3)
-
-    def install_required(package, channel=0):
+    def _install_required(packages, package, channel=0, sdk=None):
         if package in packages["installed"]:
             return
         print(f"Missing '{package}', trying to install now..")
         if package in packages["available"]:
             print(f"installing '{package}'")
-            run_command(command=["sdkmanager", package, f"--channel={str(channel)}"], print_stdout=True, catch_stdout=False)
+            application = os.path.join(sdk, 'cmdline-tools', 'latest', 'bin', 'sdkmanager') if sdk != None else 'sdkmanager'
+            run_command(command=[application, package, f"--channel={str(channel)}"], print_stdout=True, catch_stdout=False)
         else:
             raise Exception(f"Could not find the required sdk package '{package}'")
+    def _parse_installed_packages(channel=0, sdk=None):
+        application = os.path.join(sdk, 'cmdline-tools', 'latest', 'bin', 'sdkmanager') if sdk != None else 'sdkmanager'
+        sdkman_output = run_command([application, "--list", f"--channel={str(channel)}"], catch_stdout=True)
+        
+        packages = {'installed': {}, 'available': {}}
+        for line in [package for package in sdkman_output if package.count('|') == 3]:
+            name, version, _, location = (l.strip() for l in line.split('|'))
+            if name == "Path" or name.startswith('-'):
+                continue
+            packages['installed'][name] = {'version': version, 'location': location}
 
-    install_required("platforms;android-31")
-    install_required("cmake;3.22.1", channel=3)
-    install_required("ndk;25.0.8151533", channel=1)
+        for line in [package for package in sdkman_output if package.count('|') == 2]:
+            name, version, _ = (l.strip() for l in line.split('|'))
+            if name == "Path" or name.startswith('-'):
+                continue
+            packages['available'][name] = {'version': version }
 
-def build(directory, type='debug'):
-    commands =[]
-    if type == 'debug':
-        commands = [':app:bundleDebug']
-    elif type == 'release':
-        commands = [':app:bundleRelease']
+        return packages
 
-    run_command(['./gradlew'] + commands, directory=directory, print_stdout=True)
-
-def install(directory, type='debug'):
-    run_command(['adb', 'install', '-r', os.path.join(directory, 'app', 'build', 'outputs', 'apk', type, f'app-{type}.apk')], print_stdout=True)
-
-def run():
-    run_command(['adb', 'shell', 'am', 'start', '-n', 'com.paradigmengine.main/com.paradigmengine.main.MainActivity'], print_stdout=True)
+    def dependencies() -> list[str]:
+        return [("platforms;android-31", 0), ("cmake;3.22.1", 3), ("ndk;25.0.8151533", 1)]
     
+    def is_generated(self) -> bool:
+        return self._generated
+
+    def generate(self, overwrite=False) -> None:
+        if self._generated:
+            if not overwrite: return
+
+            shutil.rmtree(self._directory, ignore_errors=True)
+            self._generated = False
+
+        os.makedirs(self._directory, exist_ok=True)
+        if not os.path.exists(os.path.join(self._directory, 'settings.gradle')):
+            application = os.path.join(self._gradle, 'gradle')
+            run_command(command=[
+                    application, 'init',
+                    '--type', 'basic',
+                    '--dsl', 'groovy',
+                    '--project-name', "main",
+                    '--incubating'
+                ], directory=self._directory, print_stdout=True)
+
+        android_build_root = os.path.join(CURRENT_DIR, '..', 'core', 'main', 'android')
+        copy_tree(android_build_root, self._directory)
+        
+        # create the symlinks to the source code
+        cpp_dir = os.path.join(self._directory, 'main', 'src', 'main')
+        [ os.symlink(os.path.join(ROOT_DIR, dir), os.path.join(cpp_dir, dir)) for dir in ['psl', 'core', 'extern', 'cmake', 'CMakeLists.txt', 'tools'] if not os.path.exists(os.path.join(cpp_dir, dir))]
+        self._generated = True
+
+    def build(self, type="debug", arguments=None) -> None:
+        if not self._generated:
+            raise Exception("Project has not been generated yet")
+        print(f"building main-{type}.aab")
+        commands =[]
+        if type == 'debug':
+            commands = [':main:bundleDebug']
+        elif type == 'release':
+            commands = [':main:bundleRelease']
+        if arguments is not None:
+            commands.extend([f'-Pparadigm.{key}={value}' for key, value in zip(*[iter(arguments)]*2)])
+        run_command(['./gradlew'] + commands, directory=self._directory, print_stdout=True)
+        self._regenerate_apks.add(type)
+
+    def build_apks(self, type="debug") -> None:
+        if not os.path.exists(os.path.join('main', 'build', 'outputs', 'bundle', type, f'main-{type}.aab')):
+            raise Exception("No AAB generated yet, please run 'build' first")
+
+        print(f"building main-{type}.apks")
+        application = os.path.join(self._bundletool, 'bundletool')
+        run_command([
+            application,
+            'build-apks',
+            f"--bundle={os.path.join('main', 'build', 'outputs', 'bundle', type, f'main-{type}.aab')}",
+            f"--output={os.path.join('main', 'build', 'outputs', 'bundle', type, f'main-{type}.apks')}",
+            "--overwrite",
+            '--local-testing'], directory=self._directory, print_stdout=True)
+        if type in self._regenerate_apks:
+            self._regenerate_apks.remove(type)
+
+    def install(self, type='debug'):
+        if not os.path.exists(os.path.join('main', 'build', 'outputs', 'bundle', type, f'main-{type}.apks')):
+            self.build_apks(type)
+
+        if type in self._regenerate_apks:
+            print("main-{type}.apks out of date, trying to rebuild...")
+            self.build_apks(type)
+
+        print(f"installing main-{type}.apks")
+        application = os.path.join(self._bundletool, 'bundletool')
+        run_command([application, 'install-apks', f"--apks={os.path.join('main', 'build', 'outputs', 'bundle', type, f'main-{type}.apks')}"], directory=self._directory, print_stdout=True)
+
+    def run(self):
+        output = run_command(['adb' 'shell' 'pm' 'list' 'packages', 'com.paradigmengine.main'], catch_stdout=True)
+        if(len(output) != 1 or output[0] != 'com.paradigmengine.main'):
+            raise Exception("App wasn't installed, please install first")
+        
+        application = os.path.join(self._sdk, 'platform-tools', 'adb') if self._sdk != None else 'adb'
+        run_command([application, 'shell', 'am', 'start', '-n', 'com.paradigmengine.main/com.paradigmengine.main.MainActivity'], print_stdout=True)
+
+
 def main():
     parser = ArgumentParser()
     parser.add_argument("--output", default=os.path.join(ROOT_DIR, "builds", "android"))
     parser.add_argument("--sdk", default=None, help="set to override the used android sdk, falls back to path available one")
+    parser.add_argument("--gradle", default=None, help="set to override the used gradle, falls back to path available one")
+    parser.add_argument("--bundletool", default=None, help="set to override the used bundletool, falls back to path available one")
     parser.add_argument("-f", "--force", action="store_true", help="Forcibly generate the build directory even if it already exists")
     parser.add_argument("-r", "--run", action="store_true", help="run the intent after installation")
     parser.add_argument("-i", "--install", action="store_true", help="install the apk")
-    parser.add_argument("-b", "--build", action="store_true", help="build the apk")
+    parser.add_argument("-b", "--build", default=False, nargs='*', help="build the apk")
     parser.add_argument("-t", "--type", default="debug", help="type to generate")
     parser.add_argument("--purge", action="store_true", help="Delete previous install, and generate from scratch")
     args = parser.parse_args()
 
-    install_required_packages()
-    if args.purge:
-        args.force = True
-        shutil.rmtree(args.output, ignore_errors=True)
+    android = Android(args.output, sdk=args.sdk, gradle=args.gradle, bundletool=args.bundletool)
+    android.generate(overwrite=args.purge)
 
-
-    if not os.path.exists(args.output) or args.force:
-        generate(args.output)
-
-    if args.build:
-        build(args.output, args.type)
+    if isinstance(args.build, list):
+        android.build(args.type, args.build)
     if args.install:
-        install(args.output, args.type)
+        android.install(args.type)
     if args.run:
-        run()
+        android.run()
 
 if __name__ == "__main__":
     main()
