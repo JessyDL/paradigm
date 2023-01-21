7d2fc00c571f4aff475feb89d80733e178cb8484
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2023-01-21 15:32:02 +0200

    add info to psl/main.cpp

--- psl/main/main.cpp
+++ psl/main/main.cpp
@@ -1,3 +1,9 @@
+// --------------------------------------------------------------------------------------------------------------------
+// This main file exists for development purposes, here you can easily test out new features etc.. that are only
+// implemented in the [psl] library.
+// To activate this main file invoke cmake with `-DPE_DEV_MAKE_PSL_EXE=ON` and a new binary named `psl_main` will be
+// generated in the output's `bin` folder
+// --------------------------------------------------------------------------------------------------------------------
 
 int main(int argc, char* argv[]) {
 	return 0;
