f3c6d39b5f5c5961d0844ab5b9c770a21447f30e
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2024-06-11 20:26:19 +0300

    ability to specify sub-directories for cmake targets

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,17 +1,39 @@
 
-macro(set_target_output_directory target)    
-    foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
-        string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
-        string(TOLOWER ${OUTPUTCONFIG} OUTPUTCONFIG_FOLDERNAME)
-        set_target_properties(${target}
-            PROPERTIES
-            ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/lib"
-            LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/lib"
-            RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/bin")
+macro(set_target_output_directory)
+    set(oneValueArgs DIRECTORY)
+    set(multiValueArgs TARGET)
+    cmake_parse_arguments(SET_TARGET_OUTPUT_DIRECTORY "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    if(NOT DEFINED ${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY})
+        set(${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY} "")
+    endif()
+    foreach(targ ${SET_TARGET_OUTPUT_DIRECTORY_TARGET})
+        get_target_property(target_type ${targ} TYPE)
+        foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
+            string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
+            string(TOLOWER ${OUTPUTCONFIG} OUTPUTCONFIG_FOLDERNAME)
+            # mostly done so the directories don't get created in the output for binaries when unneeded.
+            if (target_type STREQUAL "EXECUTABLE")
+                set_target_properties(${targ}
+                    PROPERTIES
+                    RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
+            else()
+                set_target_properties(${targ}
+                    PROPERTIES
+                    ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
+                    LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
+                    RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
+            endif()
+        endforeach()
+        if (target_type STREQUAL "EXECUTABLE")
+            set_target_properties(${targ}
+                PROPERTIES
+                RUNTIME_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
+        else ()
+            set_target_properties(${targ}
+                PROPERTIES
+                ARCHIVE_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
+                LIBRARY_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
+                RUNTIME_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
+        endif()
     endforeach()
-    set_target_properties(${target}
-        PROPERTIES
-        ARCHIVE_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/lib"
-        LIBRARY_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/lib"
-        RUNTIME_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/bin")
 endmacro()
