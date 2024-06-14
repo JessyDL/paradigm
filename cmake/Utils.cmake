4ec0590479c1e6e0c2f7c4b2ae4cdb38bd0946e3
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2024-06-14 22:22:17 +0300

    improve: build dir will now place itself next to consuming projects

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -6,34 +6,36 @@ macro(set_target_output_directory)
     if(NOT DEFINED ${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY})
         set(${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY} "")
     endif()
-    foreach(targ ${SET_TARGET_OUTPUT_DIRECTORY_TARGET})
-        get_target_property(target_type ${targ} TYPE)
-        foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
-            string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
-            string(TOLOWER ${OUTPUTCONFIG} OUTPUTCONFIG_FOLDERNAME)
-            # mostly done so the directories don't get created in the output for binaries when unneeded.
+    if(${PE_INTERNAL_SET_BUILD_DIR})
+        foreach(targ ${SET_TARGET_OUTPUT_DIRECTORY_TARGET})
+            get_target_property(target_type ${targ} TYPE)
+            foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
+                string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
+                string(TOLOWER ${OUTPUTCONFIG} OUTPUTCONFIG_FOLDERNAME)
+                # mostly done so the directories don't get created in the output for binaries when unneeded.
+                if (target_type STREQUAL "EXECUTABLE")
+                    set_target_properties(${targ}
+                        PROPERTIES
+                        RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
+                else()
+                    set_target_properties(${targ}
+                        PROPERTIES
+                        ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
+                        LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
+                        RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
+                endif()
+            endforeach()
             if (target_type STREQUAL "EXECUTABLE")
                 set_target_properties(${targ}
                     PROPERTIES
-                    RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
-            else()
+                    RUNTIME_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
+            else ()
                 set_target_properties(${targ}
                     PROPERTIES
-                    ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
-                    LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
-                    RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} "${PE_BUILD_DIR}/${OUTPUTCONFIG_FOLDERNAME}/${ARCHI}/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
+                    ARCHIVE_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
+                    LIBRARY_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
+                    RUNTIME_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
             endif()
         endforeach()
-        if (target_type STREQUAL "EXECUTABLE")
-            set_target_properties(${targ}
-                PROPERTIES
-                RUNTIME_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
-        else ()
-            set_target_properties(${targ}
-                PROPERTIES
-                ARCHIVE_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
-                LIBRARY_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/lib/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}"
-                RUNTIME_OUTPUT_DIRECTORY "${PE_BUILD_DIR}/default/bin/${SET_TARGET_OUTPUT_DIRECTORY_DIRECTORY}")
-        endif()
-    endforeach()
+    endif()
 endmacro()
