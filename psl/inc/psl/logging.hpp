c70ef25994c25a5ff162219da5277b45ddb4b2a4
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2024-06-14 22:22:52 +0300

    improve: logging to file can now be conditional

--- psl/inc/psl/logging.hpp
+++ psl/inc/psl/logging.hpp
@@ -3,13 +3,13 @@
 
 
 #ifndef DISABLE_LOGGING
-	#define LOG(...) spdlog::get("main")->info(__VA_ARGS__)
+	#define LOG(...) spdlog::get("core")->info(__VA_ARGS__)
 	#define LOG_INFO(...) LOG(__VA_ARGS__)
-	#define LOG_ERROR(...) spdlog::get("main")->error(__VA_ARGS__)
-	#define LOG_WARNING(...) spdlog::get("main")->warn(__VA_ARGS__)
-	#define LOG_PROFILE(...) spdlog::get("main")->trace(__VA_ARGS__)
-	#define LOG_DEBUG(...) spdlog::get("main")->debug(__VA_ARGS__)
-	#define LOG_FATAL(...) spdlog::get("main")->critical(__VA_ARGS__)
+	#define LOG_ERROR(...) spdlog::get("core")->error(__VA_ARGS__)
+	#define LOG_WARNING(...) spdlog::get("core")->warn(__VA_ARGS__)
+	#define LOG_PROFILE(...) spdlog::get("core")->trace(__VA_ARGS__)
+	#define LOG_DEBUG(...) spdlog::get("core")->debug(__VA_ARGS__)
+	#define LOG_FATAL(...) spdlog::get("core")->critical(__VA_ARGS__)
 #else
 	#define TimeLog
 	#define LOG
