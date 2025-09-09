171dbbc8d3eb15b34fe744ae8d5473ca4e78bc3e
jdl <jessy.de.lannoit@gmail.com>
2025-09-09 23:36:29 +0300

    Improved static geometry performance Added new dynamic geometry pathway

--- core/inc/core/ecs/systems/geometry_instance.hpp
+++ core/inc/core/ecs/systems/geometry_instance.hpp
@@ -26,6 +26,12 @@ class geometry_instancing {
 		uint32_t id;
 	};
 
+	struct instance_id_sort {
+		inline bool operator()(const instance_id& lhs, const instance_id& rhs) const noexcept {
+			return lhs.id < rhs.id;
+		}
+	};
+
   public:
 	geometry_instancing(psl::ecs::state_t& state);
 	~geometry_instancing() = default;
@@ -41,6 +47,32 @@ class geometry_instancing {
 									psl::ecs::filter<const core::ecs::components::dynamic_tag>,
 									psl::ecs::order_by<renderer_sort, core::ecs::components::renderable>> pack);
 	*/
+	void dynamic_add(
+	  psl::ecs::info_t& info,
+	  psl::ecs::pack_indirect_full_t<
+		psl::ecs::entity_t,
+		core::ecs::components::renderable,
+		const core::ecs::components::transform,
+		psl::ecs::filter<core::ecs::components::dynamic_tag>,
+		psl::ecs::except<core::ecs::components::dont_render_tag>,
+		psl::ecs::on_combine<core::ecs::components::renderable, core::ecs::components::transform>> geometry_pack);
+
+	void dynamic_remove(
+	  psl::ecs::info_t& info,
+	  psl::ecs::pack_direct_partial_t<
+		psl::ecs::entity_t,
+		core::ecs::components::renderable,
+		const instance_id,
+		psl::ecs::filter<core::ecs::components::dynamic_tag>,
+		psl::ecs::except<core::ecs::components::dont_render_tag>,
+		psl::ecs::on_break<core::ecs::components::renderable, core::ecs::components::transform, instance_id>> pack);
+	void dynamic_update(psl::ecs::info_t& info,
+						psl::ecs::pack_indirect_partial_t<core::ecs::components::renderable,
+														  const core::ecs::components::transform,
+														  const instance_id,
+														  psl::ecs::filter<core::ecs::components::dynamic_tag>,
+														  psl::ecs::except<core::ecs::components::dont_render_tag>,
+														  psl::ecs::order_by<instance_id_sort, instance_id>> pack);
 	void dynamic_system(
 	  psl::ecs::info_t& info,
 	  psl::ecs::pack_direct_full_t<core::ecs::components::renderable,
@@ -81,6 +113,7 @@ class geometry_instancing {
 														const instance_id,
 														psl::ecs::except<core::ecs::components::transform>,
 														psl::ecs::on_remove<core::ecs::components::renderable>> pack);
+	std::mutex m_Mutex;
 	// void static_disable();
 	// void static_enable(psl::ecs::info& info, psl::ecs::pack_direct_full_t<psl::ecs::entity_t, const
 	// core::ecs::components::renderable, const instance_id, core::ecs::components::dont_render_tag> dont_render);
--- core/src/ecs/systems/geometry_instance.cpp
+++ core/src/ecs/systems/geometry_instance.cpp
@@ -19,12 +19,186 @@ geometry_instancing::geometry_instancing(psl::ecs::state_t& state) {
 	state.declare<"geometry_instancing::static_add">(psl::ecs::threading::seq, &geometry_instancing::static_add, this);
 	state.declare<"geometry_instancing::static_remove">(
 	  psl::ecs::threading::seq, &geometry_instancing::static_remove, this);
-	state.declare<"geometry_instancing::dynamic_system">(
-	  psl::ecs::threading::seq, &geometry_instancing::dynamic_system, this);
+	/*state.declare<"geometry_instancing::dynamic_system">(
+	  psl::ecs::threading::seq, &geometry_instancing::dynamic_system, this);*/
 	state.declare<"geometry_instancing::static_geometry_add">(
 	  psl::ecs::threading::seq, &geometry_instancing::static_geometry_add, this);
 	state.declare<"geometry_instancing::static_geometry_remove">(
 	  psl::ecs::threading::seq, &geometry_instancing::static_geometry_remove, this);
+	state.declare<"geometry_instancing::dynamic_add">(
+	  psl::ecs::threading::seq, &geometry_instancing::dynamic_add, this);
+	state.declare<"geometry_instancing::dynamic_remove">(
+	  psl::ecs::threading::par, &geometry_instancing::dynamic_remove, this);
+	state.declare<"geometry_instancing::dynamic_update">(
+	  psl::ecs::threading::par, &geometry_instancing::dynamic_update, this);
+}
+
+
+void geometry_instancing::dynamic_add(info_t& info,
+									  pack_indirect_full_t<entity_t,
+														   renderable,
+														   const transform,
+														   filter<dynamic_tag>,
+														   except<dont_render_tag>,
+														   on_combine<renderable, transform>> geometry_pack) {
+	if(geometry_pack.size() == 0) {
+		return;
+	}
+
+	auto instantiate = [&info](renderable& rend,
+							   const transform* first,
+							   const transform* last,
+							   entity_t* first_ent,
+							   entity_t* last_ent,
+							   uint32_t count) {
+		if(!rend.bundle || !rend.geometry || count == 0) {
+			return;
+		}
+		auto instancesID = rend.bundle->instantiate(rend.geometry, count);
+		psl::array<psl::mat4x4> modelMats {};
+		modelMats.reserve(count);
+		psl::array<instance_id> instanceIDs {};
+		instanceIDs.reserve(count);
+
+		for(auto [startIndex, endIndex] : instancesID) {
+			auto const range = endIndex - startIndex;
+			for(auto i = 0u; i < range; ++i, ++first) {
+				const psl::mat4x4 translationMat = translate(first->position);
+				const psl::mat4x4 rotationMat	 = to_matrix(first->rotation);
+				const psl::mat4x4 scaleMat		 = scale(first->scale);
+				modelMats.emplace_back(translationMat * rotationMat * scaleMat);
+				instanceIDs.emplace_back(instance_id {startIndex + i});
+			}
+
+			if(!rend.bundle->set(
+				 rend.geometry, startIndex, psl::string {core::gfx::constants::INSTANCE_MODELMATRIX}, modelMats))
+				core::log->error(
+				  "could not set the instance data for the dynamic elements in geometry: {} startIndex: {} size: "
+				  "{}",
+				  rend.geometry,
+				  startIndex,
+				  modelMats.size());
+			modelMats.clear();
+		}
+		info.command_buffer.add_components<instance_id>(psl::array_view<entity_t>(first_ent, last_ent), instanceIDs);
+	};
+
+	core::resource::handle<core::gfx::bundle> lastBundle {};
+	uint32_t count {0};
+	transform const* first {geometry_pack.get<const transform>().data()};
+	transform const* last {nullptr};
+	auto* render_it = geometry_pack.get<renderable>().data();
+	psl::array<psl::mat4x4> modelMats {};
+	psl::array<instance_id> instanceIDs {};
+	entity_t* first_ent = geometry_pack.get<entity_t>().data();
+	entity_t* last_ent {nullptr};
+	for(auto [ent, render, trns] : geometry_pack) {
+		auto bundleHandle = render.bundle;
+		last_ent		  = &ent;
+		last			  = &trns;
+		render_it		  = &render;
+		if(bundleHandle != lastBundle && lastBundle && bundleHandle && count > 0) {
+			instantiate(*render_it, first, last + 1, first_ent, last_ent + 1, count);
+			first	  = &trns;
+			count	  = 0;
+			first_ent = &ent;
+		}
+
+		lastBundle = bundleHandle;
+		++count;
+	}
+
+	if(count > 0) {
+		instantiate(*render_it, first, last + 1, first_ent, last_ent + 1, count);
+	}
+}
+
+void geometry_instancing::dynamic_remove(info_t& info,
+										 pack_direct_partial_t<entity_t,
+															   renderable,
+															   const instance_id,
+															   filter<dynamic_tag>,
+															   except<dont_render_tag>,
+															   on_break<renderable, transform, instance_id>> pack) {
+	for(auto [e, r, id] : pack) {
+		if(r.bundle && r.geometry) {
+			r.bundle->release(r.geometry, id.id);
+		}
+	}
+
+	info.command_buffer.remove_components<instance_id>(pack.get<entity_t>());
+}
+
+void geometry_instancing::dynamic_update(info_t& info,
+										 pack_indirect_partial_t<renderable,
+																 const transform,
+																 const instance_id,
+																 filter<dynamic_tag>,
+																 except<dont_render_tag>,
+																 order_by<instance_id_sort, instance_id>> pack) {
+	if(pack.empty()) {
+		return;
+	}
+	struct temp {
+		instance_id id;
+		psl::mat4x4 model;
+	};
+	std::unordered_map<psl::UID, std::unordered_map<psl::UID, std::pair<renderable*, psl::array<temp>>>> combos;
+	psl::UID lastBundleUID {};
+	psl::UID lastGeometryUID {};
+	std::pair<renderable*, psl::array<temp>>* currentArray = nullptr;
+	for(auto [r, t, id] : pack) {
+		const psl::mat4x4 translationMat = translate(t.position);
+		const psl::mat4x4 rotationMat	 = to_matrix(t.rotation);
+		const psl::mat4x4 scaleMat		 = scale(t.scale);
+		const auto modelMat				 = translationMat * rotationMat * scaleMat;
+		if(r.bundle.uid() == lastBundleUID && r.geometry.uid() == lastGeometryUID && currentArray) {
+			currentArray->second.emplace_back(temp {id, modelMat});
+		} else {
+			lastBundleUID	= r.bundle.uid();
+			lastGeometryUID = r.geometry.uid();
+			currentArray	= &combos[r.bundle][r.geometry];
+			currentArray->second.emplace_back(temp {id, modelMat});
+			if(currentArray->first == nullptr) {
+				currentArray->first = &r;
+				currentArray->second.reserve(pack.size());
+			}
+		}
+	}
+
+	psl::array<psl::mat4x4> modelMats {};
+	modelMats.reserve(pack.size());
+	for(auto& [bundleUID, geomMap] : combos) {
+		for(auto& [geometryUID, instanceData] : geomMap) {
+			std::sort(instanceData.second.begin(), instanceData.second.end(), [](const temp& lhs, const temp& rhs) {
+				return lhs.id.id < rhs.id.id;
+			});
+
+			size_t index	  = 0;
+			size_t totalCount = 0;
+			while(index < instanceData.second.size()) {
+				totalCount = index;
+				// figure out contiguous ranges
+				auto* firstModelMat = &instanceData.second[index].model;
+				auto firstId		= instanceData.second[index].id.id;
+				modelMats.emplace_back(instanceData.second[index].model);
+				while(index + 1 < instanceData.second.size() &&
+					  firstId + index + 1 == instanceData.second[index + 1].id.id) {
+					++index;
+					modelMats.emplace_back(instanceData.second[index].model);
+				}
+
+				totalCount = index - totalCount + 1;
+
+				auto scoped_lock   = std::scoped_lock(m_Mutex);
+				auto* lastModelMat = &instanceData.second[index].model;
+				instanceData.first->bundle->set(
+				  instanceData.first->geometry, firstId, core::gfx::constants::INSTANCE_MODELMATRIX, modelMats);
+				++index;
+				modelMats.clear();
+			}
+		}
+	}
 }
 
 
@@ -41,9 +215,11 @@ void geometry_instancing::dynamic_system(info_t& info,
 
 	{
 		auto renderers = geometry_pack.get<renderable>();
-		for(auto renderable : renderers)
-			if(renderable.bundle)
+		for(auto renderable : renderers) {
+			if(renderable.bundle) {
 				renderable.bundle->release_all();
+			}
+		}
 	}
 
 	core::profiler.scope_end();
@@ -128,7 +304,7 @@ void geometry_instancing::static_add(info_t& info,
 	core::profiler.scope_begin("create_all");
 	std::vector<psl::mat4x4> modelMats;
 	psl::array<entity_t> eIds;
-	eIds.resize(1);
+	psl::array<instance_id> instanceIds;
 	for(const auto& unique_bundle : UniqueCombinations) {
 		modelMats.clear();
 
@@ -141,6 +317,9 @@ void geometry_instancing::static_add(info_t& info,
 
 			uint32_t indicesCompleted = 0;
 			for(auto [startIndex, endIndex] : instancesID) {
+				eIds.reserve(endIndex - startIndex);
+				instanceIds.reserve(endIndex - startIndex);
+				modelMats.reserve(endIndex - startIndex);
 				for(auto i = startIndex; i < endIndex; ++i, ++indicesCompleted) {
 					const auto& transform = std::get<const core::ecs::components::transform&>(
 					  geometry_pack[indicesCompleted + geometryData.startIndex]);
@@ -149,13 +328,16 @@ void geometry_instancing::static_add(info_t& info,
 					const psl::mat4x4 scaleMat		 = scale(transform.scale);
 					modelMats.emplace_back(translationMat * rotationMat * scaleMat);
 
-					eIds[0] = std::get<entity_t&>(geometry_pack[indicesCompleted + geometryData.startIndex]);
-					info.command_buffer.add_components<instance_id>(eIds, instance_id {i});
+					eIds.emplace_back(std::get<entity_t&>(geometry_pack[indicesCompleted + geometryData.startIndex]));
+					instanceIds.emplace_back(instance_id {i});
 				}
+				info.command_buffer.add_components<instance_id>(eIds, instanceIds);
 				bundleHandle->set(
 				  geometryHandle, startIndex, psl::string {core::gfx::constants::INSTANCE_MODELMATRIX}, modelMats);
 
 				modelMats.clear();
+				eIds.clear();
+				instanceIds.clear();
 			}
 		}
 	}
@@ -188,16 +370,15 @@ void geometry_instancing::static_geometry_add(
 							   psl::ecs::on_add<core::ecs::components::renderable>> pack) {
 	if(pack.size() == 0)
 		return;
-	psl::array<entity_t> eIds;
-	eIds.resize(1);
+	psl::array<instance_id> instanceIds;
 	for(auto [entity, render] : pack) {
 		auto bundleHandle	= render.bundle;
 		auto geometryHandle = render.geometry;
 
 		auto instancesID = bundleHandle->instantiate(geometryHandle, 1);
-		eIds[0]			 = entity;
-		info.command_buffer.add_components<instance_id>(eIds, instance_id {instancesID[0].first});
+		instanceIds.emplace_back(instancesID[0].first);
 	}
+	info.command_buffer.add_components<instance_id>(pack.get<entity_t>(), instanceIds);
 }
 
 
